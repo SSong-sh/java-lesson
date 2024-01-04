8c3e23df5e475216b590023d1f8150ead206650f
SSong-sh <tjdgp0817@gmail.com>
2024-01-04 14:34:35 +0900

    Q001 과제

---
+++ JavaTest/src/com/test/question/Q001.java
@@ -0,0 +1,28 @@
+package com.test.question;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q001 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		//문제1. 만나이 계산
+//		
+//		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		System.out.print("태어난 년도: ");
+//		
+//		
+////		String year = reader.read();
+////		
+////		
+////		int age =  2024 - year;
+//		
+//		
+//	
+//		
+//		System.out.print(year);
+	}
+}
