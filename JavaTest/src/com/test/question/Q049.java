ad02acebcfb1913c899c82bd4d367d10a9dda1b8
SSong-sh <tjdgp0817@gmail.com>
2024-01-09 15:12:41 +0900

    Q049

---
+++ JavaTest/src/com/test/question/Q049.java
@@ -0,0 +1,33 @@
+package com.test.question;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q049 {
+
+	/*
+	 * 요구사항] 별찍기
+	 * 
+	 * - 조건 : 행의 갯수를 입력받으시오.
+	 * 
+	 * - 입력 : 행: 5
+	 * 
+	 */
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("행: ");
+		int n = Integer.parseInt(reader.readLine());
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+}
