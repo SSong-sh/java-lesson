b3925935a89e55dc855a4dea41d3a6c2e45a8252
SSong-sh <tjdgp0817@gmail.com>
2024-01-09 14:14:46 +0900

    Q041

---
+++ JavaTest/src/com/test/question/Q041.java
@@ -0,0 +1,26 @@
+package com.test.question;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q041 {
+
+public static void main(String[] args) throws NumberFormatException, IOException {
+
+	int sum =0;
+
+	for(int i=1; ;i++) {
+		System.out.print(i + " + ");
+		sum += i;
+
+		if(sum >=1000) {
+			System.out.println("\b\b= " + sum);
+			break;
+		}
+
+
+
+	}
+}
+}
