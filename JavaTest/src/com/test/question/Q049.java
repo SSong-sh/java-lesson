f3a58ecb8922357755e7ff4616f576b4705e9cbf
SSong-sh <tjdgp0817@gmail.com>
2024-01-09 15:24:49 +0900

    Q049 finish

--- JavaTest/src/com/test/question/Q049.java
+++ JavaTest/src/com/test/question/Q049.java
@@ -22,6 +22,9 @@ public class Q049 {
 		int n = Integer.parseInt(reader.readLine());
 
 		for (int i = 0; i < n; i++) {
+			for(int k=0; k<=i; k++) {
+				System.out.print(" ");
+			}
 			for (int j = i; j < n; j++) {
 				System.out.print("*");
 			}
