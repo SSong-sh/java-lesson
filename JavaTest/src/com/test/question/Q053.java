8b49d5deeef83bfa40bc22b5f66ceb188b05b882
SSong-sh <tjdgp0817@gmail.com>
2024-01-09 17:49:28 +0900

    Q053 수정중

---
+++ JavaTest/src/com/test/question/Q053.java
@@ -0,0 +1,20 @@
+package com.test.question;
+
+public class Q053 { 
+	
+	//구구단 출력
+	
+	public static void main(String[] args) {
+		
+		for(int i=2;i<6;i++) {
+			for(int j=1;j<=1;j++) {
+				System.out.printf("%d X %d = %d\t",i,j,i*j );
+			}
+		}
+		System.out.println();
+		
+		
+		
+	}
+
+}
