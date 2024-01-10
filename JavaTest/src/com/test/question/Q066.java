fae16ea44a993d329d765dcf357a720ddf091db8
SSong-sh <tjdgp0817@gmail.com>
2024-01-11 07:52:01 +0900

    Q066

--- JavaTest/src/com/test/question/Q066.java
+++ JavaTest/src/com/test/question/Q066.java
@@ -1,12 +1,36 @@
 package com.test.question;
 
 public class Q066 {
-	
+
 	public static void main(String[] args) {
+
+		int[] list = new int[6];
+		String temp = "";
+
+		for (int i = 0; i < list.length; i++) {
+
+			int random = (int) (Math.random() * 45) + 1; // *가 범위 조정 0부터 시작이라 +1
+			
+			list[i] = random;
+			
+			
+			for (int j = 0; j < i; j++) {
+				if (list[j] == list[i]) {
+					i--;
+					break;
+				}
+			}
+
+		}
+
+		temp = "[ ";
 		
-		int random = (int) (Math.random() * 45) + 1;
+		for(int i=0;i<list.length;i++) {
+			temp += list[i]+", ";
+		}
 		
-		
+		temp += "]";
+		System.out.println(temp);
 	}
 
 }
