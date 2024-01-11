71b9b04f031f1af5e8a62a754a4eb3e3b2ba8706
SSong-sh <tjdgp0817@gmail.com>
2024-01-12 07:35:44 +0900

    class 수업내용 전체(1/11)

---
+++ JavaTest/src/com/test/java/obj/Employee.java
@@ -0,0 +1,41 @@
+package com.test.java.obj;
+
+//직원, 사원 클래스
+
+public class Employee {
+	
+	private String name; // 직원명
+	private String department; // 직원명
+	
+	
+	//직속 상사정보
+	private String sname; // 직원명
+	private String sdepartment; // 직원명
+	
+	private Employee superior;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public Employee getSuperior() {
+		return superior;
+	}
+	public void setSuperior(Employee superior) {
+		this.superior = superior;
+	}
+
+	
+	
+
+}
---
+++ JavaTest/src/com/test/java/obj/Monitor.java
@@ -0,0 +1,42 @@
+package com.test.java.obj;
+
+public class Monitor {
+	
+	private String model;
+	private int size = 24;
+	private int price;
+	//private int level;
+	
+	//model
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		//유효성 검사
+		this.model = model;
+	}
+	
+	//size
+	public int getSize() {
+		return size;
+	}
+	
+	//price
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	//level
+	public int getLevel() {
+		
+		if(this.price >= 500000) {
+			return 1;
+		}else if (this.price <= 500000) {
+			return 2;
+		} else {
+			return 3;
+		}
+		
+	}
+
+}
