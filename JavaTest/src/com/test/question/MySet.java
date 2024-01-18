cc3e708db6d594a344d0a7f8ed374b92c77b395b
SSong-sh <tjdgp0817@gmail.com>
2024-01-18 13:49:00 +0900

    Q108

---
+++ JavaTest/src/com/test/question/MySet.java
@@ -0,0 +1,119 @@
+package com.test.question;
+
+public class MySet {
+	
+	private String[] list;
+	private int index;
+	private int index2;
+
+	public MySet() {
+		this.list = new String[4];
+		this.index = 0; // 데이터를 추가할 방번호
+		this.index2 = 0;
+		
+	}
+
+	public boolean add(String s) {
+
+		// 배열의 방이 모자란지 확인?
+		if (checkLength()) {
+
+			doubleList();
+		}
+
+		// append
+		this.list[this.index] = s;
+		this.index++;
+
+		return true; // false가 나면 에러가 나기 때문에 대부분 true가 반환됨
+	}
+
+
+	private void doubleList() {
+
+		// 기존 배열 > (복사 + 교체) > 새로운 배열
+
+		String[] temp = new String[this.list.length * 2];// 새로운 베열
+
+		// 깊은 복사
+		for (int i = 0; i < this.list.length; i++) {
+			temp[i] = this.list[i];
+		}
+
+		// ** 교체
+		this.list = temp; // 주소값 복사
+
+	}
+
+	private boolean checkLength() {
+
+		// 배열의 길이를 체크하는 메소드
+		if (this.index > this.list.length) { // 배열의 방이 꽉참
+			// 방이 꽉 착다.
+			return true;
+		}
+		// 방이 남아있다.
+		return false;
+	}
+
+	@Override
+	public String toString() {
+
+		String temp = "";
+
+		temp += String.format("lenght: %d\r\n", this.list.length);
+		temp += String.format("index: %d\r\n", this.index);
+		temp += "[\r\n";
+		for (int i = 0; i < this.list.length; i++) {
+			temp += String.format("   %d: %s\r\n", i, this.list[i]);
+		}
+		temp += "]\r\n";
+
+		return temp;
+	}
+
+	public int size() {
+		return this.index;
+	}
+
+	
+	public String remove(String s) {
+				
+		int index = -1;
+		
+		for(int i=0; i<this.index;i++) {
+			if(this.list[i].equals(s)) {
+				index = i;
+				break;
+			}	
+		}
+		
+		for(int i=index; i<this.list.length-1;i++) {
+			this.list[i] = this.list[i+1];
+		}
+		//인덱스도 줄여줘야 함
+		this.index--;
+		
+		return s;
+	}
+
+	//TODO HashSet 이 부분 한번 더 고민해보기
+	public boolean hasNext() {
+		if(this.index2 < this.index) {
+			return true;
+		}
+		return false;
+	}
+
+	public String next() {
+		String temp = this.list[this.index2];
+		index2++;
+		return temp;
+	}
+
+	public void clear() {
+		
+		this.index = 0;
+	}
+
+}
