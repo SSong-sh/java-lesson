e4f06a0cb4cbddad9631fc6bd88dd72e7607d072
SSong-sh <tjdgp0817@gmail.com>
2024-01-24 10:25:33 +0900

    Q118 과제 요구사항만 적어놓음

---
+++ JavaTest/src/com/test/question/Q118.java
@@ -0,0 +1,25 @@
+package com.test.question;
+
+import java.io.File;
+
+public class Q118 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 요구사항]
+		 폴더 내의 모든 파일들을 찾아 크기를 비교하고, 크기가 큰 순으로 정렬하시오.
+		 
+		 조건..
+		 자식 폴더내의 파일도 모두 검색하시오.
+         부모 자식 폴더에 상관없이 파일을 한번에 비교하시오.
+		 
+		 
+		 */
+		
+		String path = "C:\\class\\code\\파일 디렉토리 문제\\크기 정렬";
+		
+		File dir = new File(path);
+	}
+
+}
